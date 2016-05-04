3123b399d46e8f6ce9d2a50ed11ba118d4843d4b
Max Espinoza <espinm2@rpi.edu>
2016-05-04 12:35:04 -0400

    src folder

---
+++ src/parser.cpp
@@ -0,0 +1,15 @@
+/*
+ * This is a parser that will read in slimfly simulation output from ROSS and 
+ * transform them into 3D visualizable data.
+ *
+ * The data provided is thanks to Noah Wolfe at RPI
+ *
+ */
+
+#include <iostream>
+
+int main(int argc, char * argv[]){
+
+
+
+}
