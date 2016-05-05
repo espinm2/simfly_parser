b1ced853e229f25ad59c236306cecbcd2ab5c037
Max Espinoza <espinm2@rpi.edu>
2016-05-04 20:36:28 -0400

    finished parser

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # simfly_parser
-This is a simple parser to parse simfly ross simulation results
+This is a simple parser that generate 3D data points in a CSV file from  logs 
+created by Noah Wolfe's slimfly simulation on ROSS.
+
+# Folders
+The following folders have data that we are interested in looking at.
+- Min-UR-100
+- Min-UR-95
+- NonMin-UR-100
+- min-ur-100  vc-occupancy  routers 
+    - Populated with unknown(to me) occupancy data
+
+# Data Format
+The data is saved in the following format:
+
+```
+<global_time_0> <r0 send> <r0 recv> <r1 send> <r1 recv> .... <rn send> <rn recv>
+<global_time_1> <r0 send> <r0 recv> <r1 send> <r1 recv> .... <rn send> <rn recv>
+    .
+    .
+    .
+<global_time_N> <r0 send> <r0 recv> <r1 send> <r1 recv> .... <rn send> <rn recv>
+```
+
+Where ```global_time_i``` relates to global time for that line of data.
+The data is then stored in pairs for each router/terminal. 
+With first the send frequency and then receive frequency.
+
+# Parser Format
+We convert the file format above into 3D data points (time, router/terminal, freq)
+Each line contains a data point and is separated by commas.
+
+# How to use
+To use first compile with g++
+```
+g++ parser.cpp -o parser.o 
+```
+Then use the following format to run parser
+./parser.o -i <input path> -o <output send filename>  <output recv filename>
+An example would be
+```
+./parser.o -i ../data/Min-UR-100/slimfly_terminal_sends_recvs_log.txt -o send_data.log recv_data.log
+```
+
+Note there is an already parsed set of data ```send_data.log``` and ```recv_data.log```
+
+# For more information 
+For more info consult ```Noah_README.txt```
--- src/parser.cpp
+++ src/parser.cpp
@@ -6,10 +6,101 @@
  *
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <fstream> 
+#include <sstream>
+#include <string>
+#include <vector>
 
 int main(int argc, char * argv[]){
 
+    // Give the boot if not used correctly
+    if (argc != 6){
+        printf("Improper Usage!\n");
+        printf("Usage Example: ./parser -i <input_file> -o <send_output_file> <recv_output_file>\n");
+        return 0;
+    }
+
+    printf("Starting parser program\n");
+    std::ifstream inputFile;
+    inputFile.open(argv[2]);
+
+    if (!inputFile){
+        printf("Unable to open: %s\n", argv[2]);
+        return 0;
+    }
+
+    printf("Reading file: %s\n", argv[2]);
+
+    std::vector< std::vector<int> >data_send;
+    std::vector< std::vector<int> >data_recv;
+
+    std::string line;
+    int g_time;
+    int entity;
+    while (getline( inputFile, line ))
+    {
+
+        std::stringstream ss(line); 
+        std::string strNum;
+        unsigned int index = 0;
+        unsigned int entityNum = 0;
+
+        while (getline (ss, strNum, ',')){
+
+            if ( index == 0 ) {
+
+                // g_time is here
+                g_time = atoi(strNum.c_str());
+
+            } else if ( index % 2 == 1 ){
+
+                // send data
+                int rawData[] = {g_time,entityNum,atoi(strNum.c_str())}; // (x,y,z)
+                std::vector<int> dataPoint (rawData, 
+                        rawData + sizeof(rawData) / sizeof(int) ); 
+                data_send.push_back(dataPoint);
+
+            } else {
+                // recv data
+                int rawData[] = {g_time,entityNum,atoi(strNum.c_str())}; // (x,y,z)
+                std::vector<int> dataPoint (rawData, 
+                        rawData + sizeof(rawData) / sizeof(int) ); 
+                data_recv.push_back(dataPoint);
+
+                // next entity
+                entityNum++;
+            }
+
+            // incr index
+            index++;
+        }
+    }
 
 
+    // Writing out the send data first
+    printf("Writing file: %s\n",argv[4]);
+    std::ofstream outputFileSend;
+    outputFileSend.open(argv[4]);
+    outputFileSend << "global_time, entity_id, frequency\n";
+    for(int i = 0; i < data_send.size(); i++){
+        outputFileSend << data_send[i][0] << ", " << data_send[i][1] 
+            << ", " << data_send[i][2] << "\n";
+    }
+
+    // Writing out the recv data first
+    printf("Writing file: %s\n",argv[5]);
+    std::ofstream outputFileRecv;
+    outputFileRecv.open(argv[5]);
+    outputFileRecv << "global_time, entity_id, frequency\n";
+    for(int i = 0; i < data_recv.size(); i++){
+        outputFileRecv << data_recv[i][0] << ", " << data_recv[i][1] 
+            << ", " << data_recv[i][2] << "\n";
+    }
+
+    printf("Done!\n");
+
+    return 0;
 }
